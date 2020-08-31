9ee60307b16063dc703ae4ecbd5c6cc46b4e75fa
黎学浪 <lixuelang@cescit.com>
2020-08-31 08:48:31 +0800

    [update]npm run dev: 添加注释

--- build/bin/template.js
+++ build/bin/template.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const templates = path.resolve(process.cwd(), './examples/pages/template');
 
-const chokidar = require('chokidar');
+const chokidar = require('chokidar'); // 监听文件变化
 let watcher = chokidar.watch([templates]);
 
 watcher.on('ready', function() {
@@ -12,5 +12,6 @@ watcher.on('ready', function() {
 });
 
 function exec(cmd) {
+  // 创建子进程执行'npm run i18n'
   return require('child_process').execSync(cmd).toString().trim();
 }
