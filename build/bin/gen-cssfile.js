e0e3c97c01046c81b3e4fd50732c6d52f1c7f2ef
黎学浪 <lixuelang@cescit.com>
2020-10-30 17:42:46 +0800

    [update]theme-chalk下的变量，图标和全局字体

--- build/bin/gen-cssfile.js
+++ build/bin/gen-cssfile.js
@@ -1,11 +1,27 @@
+/*
+* 构建packages/theme-chalk/src/index.scss
+* */
 var fs = require('fs');
 var path = require('path');
+// 各个组件
 var Components = require('../../components.json');
 var themes = [
   'theme-chalk'
 ];
 Components = Object.keys(Components);
 var basepath = path.resolve(__dirname, '../../packages/');
+
+/*
+* nodejs使用stats对象来代表一个文件或设备信息，stats对象有如下方法：
+* stats.isFile()	如果是文件返回 true，否则返回 false。
+* stats.isDirectory()	如果是目录返回 true，否则返回 false。
+* stats.isBlockDevice()	如果是块设备返回 true，否则返回 false。
+* stats.isCharacterDevice()	如果是字符设备返回 true，否则返回 false。
+* stats.isSymbolicLink()	如果是软链接返回 true，否则返回 false。
+* stats.isFIFO()	如果是FIFO，返回true，否则返回 false。FIFO是UNIX中的一种特殊类型的命令管道。
+* stats.isSocket()	如果是 Socket 返回 true，否则返回 false。
+*
+* */
 
 function fileExists(filePath) {
   try {
@@ -17,8 +33,10 @@ function fileExists(filePath) {
 
 themes.forEach((theme) => {
   var isSCSS = theme !== 'theme-default';
+  // scss就引入scss，css就引入css
   var indexContent = isSCSS ? '@import "./base.scss";\n' : '@import "./base.css";\n';
   Components.forEach(function(key) {
+    // 这三个不引入
     if (['icon', 'option', 'option-group'].indexOf(key) > -1) return;
     var fileName = key + (isSCSS ? '.scss' : '.css');
     indexContent += '@import "./' + fileName + '";\n';
--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -2,9 +2,10 @@
 
 const { series, src, dest } = require('gulp');
 const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cssmin = require('gulp-cssmin');
+const autoprefixer = require('gulp-autoprefixer'); // 添加css浏览器兼容前缀
+const cssmin = require('gulp-cssmin');  // 压缩
 
+// 1.把packages/theme-chalk/src下的各scss文件分别用sass插件处理成css文件并压缩，输出到packages/theme-chalk/src/lib目录下；
 function compile() {
   return src('./src/*.scss')
     .pipe(sass.sync())
@@ -16,6 +17,7 @@ function compile() {
     .pipe(dest('./lib'));
 }
 
+// 2.把packages/theme-chalk/src/fonts下的字体文件经过压缩处理，同样输出到packages/theme-chalk/src/lib目录下；
 function copyfont() {
   return src('./src/fonts/**')
     .pipe(cssmin())
